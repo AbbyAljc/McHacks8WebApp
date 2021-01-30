aa6c1a951c0adedfb7cc97fed342eca1eed463a8
Salam81 <14456384+Salam81@users.noreply.github.com>
2021-01-30 16:54:03 -0500

    Login

---
+++ assets/custom/js/auth.js
@@ -0,0 +1,95 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDhgp_fbMSpRiYBZYXgA09l9hh5wckz34w",
+    authDomain: "mchacks2021-39752.firebaseapp.com",
+    projectId: "mchacks2021-39752",
+    storageBucket: "mchacks2021-39752.appspot.com",
+    messagingSenderId: "478478347728",
+    appId: "1:478478347728:web:3c3bef39b903026022ddd5",
+    measurementId: "G-H3L6J6QE5X"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+init();
+
+
+function init() {
+    var path = window.location.pathname.split('/');
+    const currentPath = path[path.length - 1].split('.')[0];
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+
+            var db = firebase.firestore();
+            var users = db.collection("users").doc(user.uid);
+
+            users.get().then(function(doc) {
+                if (doc.exists) {
+                  if(doc.data().type == 'patient'){
+                    $("#errorMsg").text('')
+                    $("#errorMsg").text("This is for Doctors and Pharmacists only. Please use the application.")
+                  }else{
+                    if (currentPath === 'login') window.location.href = "./dashboard.html";
+
+                    $("#userName").text(doc.data().first_name + " " + doc.data().last_name);
+                    $("#userIcon").attr('src', doc.data().icon);
+                    $("#userIcon").attr('hidden', false);
+
+                  }
+                } else {
+                    console.log("No such document!");
+                    window.location.href = "./login.html";
+                }
+            }).catch(function(error) {
+                console.log("Error getting document:", error);
+                window.location.href = "./login.html";
+            });
+        } else {
+            console.log("Logged out");
+            if (currentPath !== 'login')
+                window.location.href = "./login.html";
+        }
+    });
+}
+
+
+function LoginPage() {
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start('#firebaseui-auth-container', {
+        callbacks: {
+            signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+                return true;
+            },
+            uiShown: function() {
+                document.getElementById('loader').style.display = 'none';
+            }
+        },
+        signInFlow: 'popup',
+        // signInSuccessUrl: 'home.html',
+        signInOptions: [
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        ],
+    });
+}
+
+$("#emailSignIn").submit(function(event) {
+    event.preventDefault();
+    var path = window.location.pathname.split('/');
+    const currentPath = path[path.length - 1].split('.')[0];
+
+    firebase.auth().signInWithEmailAndPassword($("#emailLogin").val(), $("#passwordLogin").val())
+        .then((userCredential) => {
+            console.log("logging in")
+        })
+        .catch((error) => {
+            $("#errorMsg").text('')
+            $("#errorMsg").text(error.message)
+        });
+});
+
+function logout() {
+    firebase.auth().signOut().then(() => {
+        window.location.href = "./login.html";
+    }).catch((error) => {
+        alert(error)
+    });
+}
